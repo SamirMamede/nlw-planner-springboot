49322b69c769427f94a841d2aa2928f12e026869
SamirMamede <anuarsamir@gmail.com>
2024-07-27 01:37:05 -0400

    endpoint to query trip data

--- src/main/java/com/nlw/planner/trip/TripController.java
+++ src/main/java/com/nlw/planner/trip/TripController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/trips")
 public class TripController {
@@ -24,5 +27,12 @@ public class TripController {
         this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
