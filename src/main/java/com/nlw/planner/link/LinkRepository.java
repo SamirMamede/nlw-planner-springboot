ebfe15f794a2ed9e79c897468a39c4038d9a0733
SamirMamede <anuarsamir@gmail.com>
2024-08-26 21:30:30 -0400

    create link repository

---
+++ src/main/java/com/nlw/planner/link/LinkRepository.java
@@ -0,0 +1,8 @@
+package com.nlw.planner.link;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LinkRepository extends JpaRepository<Link, UUID> {
+}
